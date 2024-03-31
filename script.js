677cf571db57124a6942e6f94ebf01b27779b290
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-31 16:19:15 -0700

    Working on js function, in progress still, styling changes isn't working yet

--- script.js
+++ script.js
@@ -25,29 +25,19 @@ let checking = true;
 function seeMoreLess(idNumber){
     divName = "seeMoreLess" + idNumber.toString();
     seeName = "seeFunction" + idNumber.toString();
-    console.log("Element with id " + seeName + " is : ");
-    console.log(document.getElementById(seeName));
-    // console.log("style is : " + document.getElementById(divName).style);
-    // console.log(getComputedStyle(document.getElementById(divName)));
-    // if(checking = true){
-    //     document.getElementById(seeName).innerText = "See Less..."
-    //     console.log(checking);
-    //     checking = false;
-    //     // document.getElementById(divName).style.display = "block";
-    // } else if(checking = false) {
-    //     document.getElementById(seeName).innerText = "See More..."
-    //     console.log(checking);
-    //     checking = true;
-    //     // document.getElementById(divName).style.display = "none";
-    // } else {
-    //     document.getElementById(seeName).innerText = "Nice! You broke the code :D"
-    // }
-    if(document.getElementById(seeName).innerText = "See More..."){
-        document.getElementById(seeName).innerText = "See Less..."
-        // document.getElementById(divName).style.display = "block";
-    } else if(document.getElementById(seeName).innerText = "See Less...") {
-        document.getElementById(seeName).innerText = "See More..."
-        // document.getElementById(divName).style.display = "none";
+    // console.log("Element with id " + seeName + " is : ");
+    // console.log(document.getElementById(seeName));
+    // LOOK INTO Document selector query all 
+    if(document.getElementById(seeName).innerText == "See More..."){
+        document.getElementById(seeName).innerText = "See Less...";
+        console.log(divName);
+        console.log(document.getElementById(divName));
+        document.querySelector('seeFunction').previousElementSibling.style.display = "block";
+        // document.getElementById(divName).style.display = block;
+        
+    } else if(document.getElementById(seeName).innerText == "See Less...") {
+        document.getElementById(seeName).innerText = "See More...";
+        document.getElementById(divName).style.display = none;
     } else {
         document.getElementById(seeName).innerText = "Nice! You broke the code :D"
     }
