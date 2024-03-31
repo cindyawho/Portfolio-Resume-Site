9c10e768089106a4fbb0800f4cb12fde81c15f09
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-31 16:50:42 -0700

    See More Less Function now works for this div!!

--- script.js
+++ script.js
@@ -20,25 +20,20 @@ function hireButton(){
 }
 
 
-let checking = true;
-
-function seeMoreLess(idNumber){
-    divName = "seeMoreLess" + idNumber.toString();
-    seeName = "seeFunction" + idNumber.toString();
-    // console.log("Element with id " + seeName + " is : ");
-    // console.log(document.getElementById(seeName));
-    // LOOK INTO Document selector query all 
-    if(document.getElementById(seeName).innerText == "See More..."){
-        document.getElementById(seeName).innerText = "See Less...";
-        console.log(divName);
-        console.log(document.getElementById(divName));
-        document.querySelector('seeFunction').previousElementSibling.style.display = "block";
-        // document.getElementById(divName).style.display = block;
-        
-    } else if(document.getElementById(seeName).innerText == "See Less...") {
-        document.getElementById(seeName).innerText = "See More...";
-        document.getElementById(divName).style.display = none;
+function seeMoreLess(thisElem){
+    // console.log(thisElem);
+    // console.log(thisElem.innerText);
+    // // let sibling = document.querySelector(thisElem).previousElementSibling;
+    // // console.log("Sibling is: ");
+    // // console.log(sibling);
+    // console.log(thisElem.previousElementSibling);
+    if(thisElem.innerText=="See More..."){
+        thisElem.innerText = "See Less...";
+        thisElem.previousElementSibling.style.display = "block";
+    } else if(thisElem.innerText=="See Less..."){
+        thisElem.innerText = "See More...";
+        thisElem.previousElementSibling.style.display = "none";
     } else {
-        document.getElementById(seeName).innerText = "Nice! You broke the code :D"
+        thisElem.innerText= "Congrats! You broke the code :) "
     }
 }
